ddb1220aa9532d4db2c71f967a7abd9b74784caa
Jack Tierney <jack.tierney@student.ucc.ie>
2023-02-14 10:34:41 +0000

    return biosample metadata

--- scripts/fetch_sample_metadata.py
+++ scripts/fetch_sample_metadata.py
@@ -13,44 +13,48 @@ import argparse
 from Bio import Entrez
 import pandas as pd
 
-def run_search_biosample(term: str, retmax: int=10) -> list:
+def run_search_biosample(term: str, retmax: int=10) -> dict:
     '''
-    return sample information for a specific sample
+    Return sample information for a specific sample
     Parameters:
-        term (str): Search term to run on NCBI GEO
+        term (str): Search term to run on biosample (GSM ID, SRA ID, etc.)
         retmax (int): Maximum number of results to return
 
     Returns:
         gds_ids (list): List of GDS IDs
     '''
     Entrez.email = "riboseq@gmail.com"
-    handle = Entrez.esearch(db="bioproject", term=term, retmax=retmax)
+    handle = Entrez.esearch(db="biosample", term=term, retmax=retmax)
 
     biosample_ids = Entrez.read(handle)["IdList"]
-    print(biosample_ids)
-    
-    for i in biosample_ids:
-        handle = Entrez.esummary(db="bioproject", id=i, retmax=1)
+
+    for id in biosample_ids:
+        handle = Entrez.esummary(db="biosample", id=id, retmax=1)
         records = Entrez.read(handle)
-        print(records)
-    return biosample_ids
+
+    return dict(records['DocumentSummarySet']['DocumentSummary'][0])
 
 
 def main(args):
 
     if args.GSE:
-        run_search_biosample(args.GSE)
+        records = run_search_biosample(args.GSE)
     elif args.SRP:
-        run_search_biosample(args.SRP)
+        records = run_search_biosample(args.SRP)
 
-
-    return False
+    if records:
+        records = {k: [v] for k, v in records.items()}
+        records_df = pd.DataFrame.from_dict(records)
+        records_df.to_csv( f'{args.output}', index=True)
+    else:
+        raise Exception("No records found")
 
 if __name__ == "__main__":
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Fetch sample information')
     parser.add_argument('--GSE', type=str, help='Studies GSE accession number.')
-    parser.add_argument('--SRP', type=str, help='Sample SRP accession number.')
+    parser.add_argument('--SRP', type=str, help='Study SRP accession number.')
+    parser.add_argument('--output', type=str, help='Output file name.')
 
     args = parser.parse_args()
     main(args)
